8c197e06707c4cc83984e5ceedb16d419fb70347
Hiếu <thehieu090107@gmail.com>
2026-04-20 15:53:39 +0700

    Add files via upload

--- btth.sql
+++ btth.sql
@@ -56,7 +56,7 @@ FROM orders
 WHERE total_amount BETWEEN 2000000 AND 5000000
   AND status <> 'CANCELLED'
   AND (
-        note LIKE N'%gấp%'   -- hỗ trợ unicode
+        note LIKE N'%gấp%'   
         OR user_id IS NULL
       )
 ORDER BY total_amount DESC
